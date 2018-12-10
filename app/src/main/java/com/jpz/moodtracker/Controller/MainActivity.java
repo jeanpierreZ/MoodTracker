a058a1d2d3b6e494173fd6fa0e1813e315298f51
jpz <jeanpierrezingraff@gmail.com>
2018-12-10 22:02:14 +0100

    Architecture MVC Add res : drawable (dpi) & values (colors)

--- app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jpz.moodtracker;
+package com.jpz.moodtracker.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jpz.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
