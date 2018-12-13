9ff2a8801c519c824f73f41903000fc584e5ef10
jpz <jeanpierrezingraff@gmail.com>
2018-12-13 16:52:22 +0100

    Supp NoteActivity and its layout Replace by an AlertDialog to save the comment in MainActivity

--- app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
@@ -1,11 +1,16 @@
 package com.jpz.moodtracker.Controller;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.jpz.moodtracker.R;
 
@@ -40,10 +45,32 @@ public class MainActivity extends AppCompatActivity {
 
         maddNote.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // User clicked the button
-                Intent noteActivityIntent = new Intent(MainActivity.this, NoteActivity.class);
-                startActivity(noteActivityIntent);
+            public void onClick(View w) {
+                AlertDialog.Builder note = new AlertDialog.Builder(MainActivity.this);
+                note.setTitle("Commentaire");
+                final EditText input = new EditText(MainActivity.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                note.setView(input);
+
+                note.setPositiveButton("VALIDER", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+
+                        //a toast to control if the string in the EditText is OK before to save it
+                        Toast.makeText(getApplicationContext(), input.getText(), Toast.LENGTH_LONG).show();
+                    }
+                });
+                note.setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                note.show();
             }
         });
     }
