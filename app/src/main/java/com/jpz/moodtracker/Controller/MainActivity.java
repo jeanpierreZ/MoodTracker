946e798500b1b7c28ea8df836c71d2263e7c528e
jpz <jeanpierrezingraff@gmail.com>
2018-12-17 11:00:28 +0100

    Display Supper Happy Mood when slide up Display Normal Mood when slide down

--- app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/jpz/moodtracker/Controller/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -16,10 +19,17 @@ import com.jpz.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View mbackgroundColor;
-    private ImageView msmileyMood;
+    //private View mbackgroundColor;
+    //private ImageView msmileyMood;
     private Button mconsultHistory;
+
     private Button maddNote;
+
+    private FrameLayout msuperHappyLayout;
+    private FrameLayout mhappyLayout;
+    private FrameLayout mnormalLayout;
+
+    private GestureDetector mGestureDetector;
 
     private SharedPreferences mPreferences;
 
@@ -30,8 +40,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mbackgroundColor = (View) findViewById(R.id.activity_main_background_color);
-        msmileyMood = (ImageView) findViewById(R.id.activity_main_smiley);
+        AndroidGestureDetector androidGestureDetector = new AndroidGestureDetector();
+        mGestureDetector = new GestureDetector(MainActivity.this, androidGestureDetector);
+
+        msuperHappyLayout = (FrameLayout) findViewById(R.id.activity_main_superHappy);
+        mhappyLayout = (FrameLayout) findViewById(R.id.activity_main_happy);
+        mnormalLayout = (FrameLayout) findViewById(R.id.activity_main_normal);
+
+        //mbackgroundColor = (View) findViewById(R.id.activity_main_background_color);
+        //msmileyMood = (ImageView) findViewById(R.id.activity_main_smiley);
         maddNote = (Button) findViewById(R.id.activity_main_note_add);
         mconsultHistory = (Button) findViewById(R.id.activity_main_history);
 
@@ -78,5 +95,65 @@ public class MainActivity extends AppCompatActivity {
                 note.show();
             }
         });
+    }
+
+    private void changeUp() {
+        msuperHappyLayout.setVisibility(View.VISIBLE);
+        mhappyLayout.setVisibility(View.INVISIBLE);
+        mnormalLayout.setVisibility(View.INVISIBLE);
+    }
+
+    private void changeDown() {
+        mnormalLayout.setVisibility(View.VISIBLE);
+        mhappyLayout.setVisibility(View.INVISIBLE);
+        msuperHappyLayout.setVisibility(View.INVISIBLE);
+    }
+
+
+    class AndroidGestureDetector implements GestureDetector.OnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            // Swipe the screen down to up
+            if (e1.getY() > e2.getY()) {
+                changeUp();
+            }
+
+            // Swipe the screen up to down
+            if (e1.getY() < e2.getY()) {
+                changeDown();
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
